a5e36167891c80bad64165b85b16d8ac187f9d48
annakassai <74617784+annakassai@users.noreply.github.com>
2021-03-21 15:20:12 +0100

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.jpg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo img-fluid mt-5" alt="logo" />
-      Hello
-      </div>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid mt-5" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Anna Kassai
+        </footer>
+      </div> 
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react"; 
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    const [keyword, setKeyword] = useState(""); 
+
+    function handleResponse(response) {
+        console.log(response.data[0]);
+
+    }
+
+    function search(event) {
+        event.preventDefault();
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`; 
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleKeywordChange (event) {
+        setKeyword(event.target.value);
+    }
+
+    return (
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input 
+            type="search" onChange={handleKeywordChange}
+            />
+        </form>
+    </div>
+    );
+}
