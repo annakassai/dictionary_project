1c0de77498fdd2be9881ee4f8c4b27859106fca9
annakassai <74617784+annakassai@users.noreply.github.com>
2021-03-21 14:25:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.jpg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo img-fluid mt-5" alt="logo" />
+      Hello
+      </div>
+    </div>
+  );
+}
